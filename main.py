3b8d6e3963cbeb9bd783cbca2e9c6c5b2385c571
Leonardo Forgiarini Guedes <43677896+leonfogue@users.noreply.github.com>
2022-03-28 11:28:54 -0300

    Update main.py
    Relacionei os valores dos sentimentos e das reações. Assim, quando na Tela.py o usuário aponta como estava se sentindo, o programa reconhecerá naquela resposta um valor e então aplicará ela ao estado emocional (self.sentimento). Também ajustei a resposta do programa ao estado emocional com as palavras Adorando, Curtindo, Odiando e Relação conturbada.

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 """
-O FEEL é um app/site que permite você reagir a coisas de seu dia. Por exemplo, você é fã de um time de futebol e
-em um Domingo qualquer, você assiste uma partida que seu time perde de goleada.
-Você reage no FEEL, dizendo como está se sentindo em relação ao time naquele instante.
-O FEEL responderá depois como está sua relação com o time.
+O FEEL é um app/site que permite você reagir a coisas de seu dia.
+Por exemplo, você é fã de um time de futebol e em um Domingo qualquer, assiste uma partida que seu time perde de goleada.
+Você reage no FEEL, dizendo como está se sentindo em relação ao time naquele instante. O FEEL responderá depois como está sua relação com o time.
 """
 
 class Feel:
-    def __init__(self, nome):
+    def __init__(self,nome):
         self.nome = nome
         self.sentimento = 50
 
 
+    def react(self,react):
+        if react == 'Bem':
+            self.sentimento += 12
+        if react == 'Muito bem':
+            self.sentimento += 18
+        if react == 'Neutro':
+            self.sentimento -= 7
+        else:
+            if react == 'Mal':
+                self.sentimento -= 15
+            if react == 'Péssimo':
+                self.sentimento -= 20
+
+
+    def show(self):
+        if self.sentimento >= 65:
+            return 'Adorando'
+        if self.sentimento >= 50:
+            return 'Curtindo'
+        if self.sentimento <= 30:
+            return 'Odiando'
+        else:
+            return 'Relação conturbada'
+        
