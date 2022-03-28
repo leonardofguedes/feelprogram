5a56e96a8bced653f16bdf54479521a19598df15
Leonardo Forgiarini Guedes <43677896+leonfogue@users.noreply.github.com>
2022-03-28 11:23:07 -0300

    Update Tela.py
    Add sleep function and a few others ajusts.

--- Tela.py
+++ Tela.py
@@ -1,28 +1,42 @@
 from main import Feel
+from time import sleep
+
+def carregando():
+    tempo = '.'*50
+    for c in tempo:
+        print(c, end='')
+        sleep(0.2)
+    print('')
 
 reacts = ['Péssimo','Mal','Neutro','Bem','Muito bem']
 
-while True:
-    assunto = str(input('Qual é o assunto que você deseja reagir?')).strip().capitalize()
-    x = Feel(assunto)
-    resposta = False
-    while resposta == False:
-        react = str(input(f'Como você se sentiu em relação a(o) {x.nome} hoje?\n[Péssimo][Mal][Neutro][Bem][Muito bem]')).strip().capitalize()
-        if react not in reacts:
-            print('Você deve falar uma opção listada.\nObs: Cuide os acentos')
+def tela():
+    while True:
+        assunto = str(input('Qual é o assunto que você deseja reagir?')).strip().capitalize()
+        x = Feel(assunto)
+        resposta = False
+        while resposta == False:
+            r = str(input(f'Como você se sentiu em relação a(o) {x.nome} hoje?\n[Péssimo][Mal][Neutro][Bem][Muito bem]')).strip().capitalize()
+            x.react(r)
+            if r not in reacts:
+                print('Você deve falar uma opção listada.\nObs: Cuide os acentos')
+            else:
+                resposta = True
+        continuar = input(f'Você deseja saber o seu sentimento pelo {x.nome}? [S][N]').strip().upper()
+        if continuar == 'S':
+            print(f'Seu sentimento pelo {x.nome} é:')
+            carregando()
+            print(f'<<<{x.show()}>>>')
+            repeat = str(input('Quer continuar? [S][N]')).strip().upper()
+            if repeat == 'N':
+                break
+            else:
+                pass
         else:
-            resposta = True
-    continuar = input(f'Você deseja saber o seu sentimento pelo {x.nome}? [S][N]').strip().upper()
-    if continuar == 'S':
-        print(f'Seu sentimento pelo {x.nome} é de {x.sentimento}.')
-        repeat = str(input('Quer continuar? [S][N]')).strip().upper()
-        if repeat == 'N':
-            break
-        else:
-            pass
-    else:
-        repeat = str(input('Quer continuar? [S][N]')).strip().upper()
-        if repeat == 'N':
-            break
-        else:
-            pass
+            repeat = str(input('Quer continuar? [S][N]')).strip().upper()
+            if repeat == 'N':
+                break
+            else:
+                pass
+
+tela()
