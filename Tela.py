1fc0b43cb7ab1f3f8dd8475a31d03bf866b5fcb1
Leonardo Forgiarini Guedes <43677896+leonfogue@users.noreply.github.com>
2022-03-29 18:00:13 -0300

    Update Tela.py

--- Tela.py
+++ Tela.py
@@ -1,42 +1,70 @@
 from main import Feel
 from time import sleep
 
-def carregando():
+reacts = ['Péssimo', 'Mal', 'Neutro', 'Bem', 'Muito bem'] #reacts permitidas no programa
+
+def carregando(): #função de carregar a página (estilo apenas)
     tempo = '.'*50
     for c in tempo:
         print(c, end='')
         sleep(0.2)
     print('')
 
-reacts = ['Péssimo','Mal','Neutro','Bem','Muito bem']
+lista_assuntos = [] #lista de assuntos reagidos
+
+
+def procurar_assunto_por_nome(nome): #função para conferir se o assunto já foi reagido anteriormente (se está na lista)
+    for assunto in lista_assuntos:
+        if assunto.nome == nome:
+            return assunto
+
+
+
+def receber_react(nome): #função captar a reação do cliente
+    while True:
+        r = input(f'Como você se sentiu em relação a(o) {nome} hoje?\n[Péssimo][Mal][Neutro][Bem][Muito bem] ').strip().capitalize()
+        if r not in reacts:
+            print('Você deve escolher uma das opções listadas.\nObs: Cuide os acentos')
+        else:
+            return r
+
+
+def mostra_sentimento(assunto): #função mostrar o sentimento do cliente
+    print(f'Seu sentimento pelo {assunto.nome} é:')
+    carregando()
+    print(f'<<< {assunto.show()} >>>')
 
 def tela():
-    while True:
-        assunto = str(input('Qual é o assunto que você deseja reagir?')).strip().capitalize()
-        x = Feel(assunto)
-        resposta = False
-        while resposta == False:
-            r = str(input(f'Como você se sentiu em relação a(o) {x.nome} hoje?\n[Péssimo][Mal][Neutro][Bem][Muito bem]')).strip().capitalize()
-            x.react(r)
-            if r not in reacts:
-                print('Você deve falar uma opção listada.\nObs: Cuide os acentos')
-            else:
-                resposta = True
-        continuar = input(f'Você deseja saber o seu sentimento pelo {x.nome}? [S][N]').strip().upper()
-        if continuar == 'S':
-            print(f'Seu sentimento pelo {x.nome} é:')
-            carregando()
-            print(f'<<<{x.show()}>>>')
-            repeat = str(input('Quer continuar? [S][N]')).strip().upper()
-            if repeat == 'N':
-                break
-            else:
-                pass
-        else:
-            repeat = str(input('Quer continuar? [S][N]')).strip().upper()
-            if repeat == 'N':
-                break
-            else:
-                pass
 
-tela()
+    while True:
+        nome = input('Qual é o assunto que você deseja reagir?').strip().capitalize()
+        assunto = procurar_assunto_por_nome(nome)
+
+        if assunto:
+            print('Você já interagiu sobre esse assunto antes.')
+            continuar = input(f'Você quer avaliar o {assunto} novamente? [S][N] ').upper().strip()
+            if continuar == 'S':
+                r = receber_react(nome)
+                assunto.react(r)
+
+                print('Recebemos sua nova reação.')
+
+                continuar = input(f'Você deseja saber o seu sentimento pelo {assunto}? [S][N] ').strip().upper()
+                if continuar == 'S':
+                    mostra_sentimento(assunto)
+
+
+        else:
+            assunto = Feel(nome)
+            lista_assuntos.append(assunto)
+
+            r = receber_react(nome)
+            assunto.react(r)
+
+            continuar = input(f'Você deseja saber o seu sentimento pelo {assunto.nome}?[S][N]').strip().upper()
+            if continuar == 'S':
+                mostra_sentimento(assunto)
+            else:
+                assuntos = [f'{a}' for a in lista_assuntos]
+                print(f'Suas reações são sobre os seguintes assuntos:\n{assuntos}')
+
